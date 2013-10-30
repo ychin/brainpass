963ca1080ff6eafef4a6028194cf9ee0b5b93c7c
Yee Cheng Chin <ychin.git@gmail.com>
2013-10-30 08:01:22 -0700

    Cleanup and made the code web worker clean. If the code is in an environment where web workers are available (note that in Chrome they aren't when in file://) will use those to generate keys instead to prevent stalling the UI for 2 seconds. Still need to make sure we don't keep sending messages over to generate more when we're already busy since right now they pile up.

--- js/brainpass.js
+++ js/brainpass.js
@@ -1,41 +1,31 @@
-(function($){
+(function(global){
     var kPbkdf2Iteration = 300000;
     var kTimeoutDuration = 600;
 
     var timeout = null;
 
-    function showHidePassphrase() {
-        var pass = $('#passphrase');
-        if (pass.attr('type') == 'password') {
-            pass.attr('type', 'text');
-            $('#hidePassphrase').html('Hide');
-        }
-        else {
-            pass.attr('type', 'password');
-            $('#hidePassphrase').html('Show');
-        }
-    }
-
-    var numBits = 256; // this needs to actually be dependent on desired key size, should make at least 256
-    function GeneratePassword() {
-        var passphrase = $('#passphrase').val();
-        var sitename = $('#sitename').val();
-        var siteusername = $('#siteusername').val();
-        var customSalt = $('#customSalt').val();
-
+    // Crypto
+    var numCryptoBits = 256; // this just needs to be large enough to cover enough bits for the password. Assuming an alphanumeric password with roughly 6 bits per char, this can generate 42 unique password characters
+    function GeneratePassword(passphrase, sitename, siteusername, customSalt) {
+        // Generate the PBKDF2 hash first with all information taken into account, then use that hash to generate
+        // the per-site password from the hash which is what we want.
         var salt = sjcl.hash.sha256.hash('brainpassSalt' + sitename + siteusername + customSalt);
-        var passphraseHash = sjcl.misc.pbkdf2(passphrase, salt, kPbkdf2Iteration, numBits);
+        var passphraseHash = sjcl.misc.pbkdf2(passphrase, salt, kPbkdf2Iteration, numCryptoBits);
         var hashString = sjcl.codec.hex.fromBits(passphraseHash);
 
-        $('#hash').val(hashString);
-        GenerateSitePassword(passphraseHash);
-        ShowVerifier(hashString);
+        var passwordStr = GenerateSitePassword(passphraseHash);
+        var verifierHex = ShowVerifier(hashString);
+
+        return {
+            hashString: hashString,
+            passwordStr: passwordStr,
+            verifierHex: verifierHex
+        };
     }
 
     function GenerateSitePassword(hash) {
-        var alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        var passwordLength = 20;
-        var bitsLeft = numBits;
+        var alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var passwordLength = 24;
         var base = BigInteger.valueOf(alphabet.length);
         var password = new Array(passwordLength);
         var hashString = sjcl.codec.hex.fromBits(hash);
@@ -47,24 +37,103 @@
             hashBigNum = hashBigNum.subtract(mod).divide(base);
         }
         var passwordStr = password.join('');
-        $('#generatedPassword').val(passwordStr);
+        return passwordStr;
     }
 
     function ShowVerifier(hashString) {
         var verifierBytes = sjcl.hash.sha256.hash(hashString);
         var verifierHex = sjcl.codec.hex.fromBits(verifierBytes);
-        $('#verifier').val(verifierHex);
+        return verifierHex;
+    }
+
+    // Web Worker async
+    var asyncWorker;
+    function SetupWebWorker() {
+        if (Worker === undefined) {
+            return;
+        }
+
+        try {
+            asyncWorker = new Worker('js/brainpass.js');
+        }
+        catch (e) {
+            asyncWorker = undefined;
+            return;
+        }
+
+        asyncWorker.postMessage({type:'load'}); // this will start the worker
+        asyncWorker.onmessage = function(message) {
+            ShowResults(message.data);
+            HideProgressBar();
+        };
+    }
+
+    onmessage = function(message) { // these are only called when in the worker thread
+        if (message.data.type == 'load') {
+            window = global;
+            importScripts('bitcoinjs-min.js', 'http://crypto.stanford.edu/sjcl/sjcl.js');
+        }
+        else if (message.data.type == 'generate') {
+            var passwordResults = GeneratePassword(message.data.data.passphrase, message.data.data.sitename, message.data.data.siteusername, message.data.data.customSalt);
+            postMessage(passwordResults);
+        }
+    };
+
+    // UI
+    function ShowProgressBar() {
+        $('#generateProgressBar').toggle(true);
+    }
+    function HideProgressBar() {
+        $('#generateProgressBar').toggle(false);
+    }
+
+    function ShowResults(results) {
+        $('#hash').val(results.hashString);
+        $('#generatedPassword').val(results.passwordStr);
+        $('#verifier').val(results.verifierHex);
+    }
+
+    function ShowHidePassphrase() {
+        var pass = $('#passphrase');
+        if (pass.attr('type') == 'password') {
+            pass.attr('type', 'text');
+            $('#hidePassphrase').html('Hide');
+        }
+        else {
+            pass.attr('type', 'password');
+            $('#hidePassphrase').html('Show');
+        }
     }
 
     function OnInputChange() {
         clearTimeout(timeout);
         timeout = setTimeout(function() {
-            GeneratePassword();
+            var passphrase = $('#passphrase').val();
+            var sitename = $('#sitename').val();
+            var siteusername = $('#siteusername').val();
+            var customSalt = $('#customSalt').val();
+
+            if (asyncWorker) {
+                ShowProgressBar();
+                asyncWorker.postMessage({
+                    type: 'generate',
+                    data: {
+                        passphrase: passphrase,
+                        sitename: sitename,
+                        siteusername: siteusername,
+                        customSalt: customSalt
+                    }
+                });
+            }
+            else {
+                var passwordResults = GeneratePassword(passphrase, sitename, siteusername, customSalt);
+                ShowResults(passwordResults);
+            }
         }, kTimeoutDuration);
     }
 
     function OnInput(id, func) {
-        $(id).bind("input keyup keydown keypress change blur", function() {
+        $(id).bind("input change", function() {
             if ($(this).val() != jQuery.data(this, "lastvalue")) {
                 func();
             }
@@ -75,13 +144,16 @@
         });
     }
 
-    $(document).ready( function() {
-        
-        var elemsNeedInput = ['#passphrase', '#sitename', '#siteusername', '#customSalt'];
-        for (var i = 0; i < elemsNeedInput.length; ++i) {
-            OnInput(elemsNeedInput[i], OnInputChange);
-        }
+    if (global.jQuery) { // this is not defined when in web worker
+        $(document).ready( function() {
+            var elemsNeedInput = ['#passphrase', '#sitename', '#siteusername', '#customSalt'];
+            for (var i = 0; i < elemsNeedInput.length; ++i) {
+                OnInput(elemsNeedInput[i], OnInputChange);
+            }
 
-        $('#hidePassphrase').click(showHidePassphrase);
-    });
-})(jQuery);
+            $('#hidePassphrase').click(ShowHidePassphrase);
+
+            SetupWebWorker();
+        });
+    }
+})(this);

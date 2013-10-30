ac724d8ecbdfe264cb0b95eab806b12ea7e157de
Yee Cheng Chin <ychin.git@gmail.com>
2013-10-30 05:39:10 -0700

    First commit for all the source files etc. Brainpass is a utility web page to generate per-site password from a master root password using PBKDF2 with high number of iterations. Started from brainwallet codebase so there are some similarities especially in the look. Will do more cleanup later

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ brainpass
 =========
 
 Utility webpage to generate per-site passwords from a master passphrase, without needing external data. Similar to how brainwallet works.
+
+TODOs:
+- CLEANUP
+- webworker to do in the background
+- is there a way to incrementally hash?
+- local storage stores websites for me
+- random passphrase generator
+ - different languages
+- verifier to make sure we typed the correct password
+- Use iframe sandbox
---
+++ js/brainpass.js
@@ -0,0 +1,79 @@
+(function($){
+    var kPbkdf2Iteration = 300000;
+    var kTimeoutDuration = 600;
+
+    var timeout = null;
+
+    function showHidePassphrase() {
+        var pass = $('#passphrase');
+        if (pass.attr('type') == 'password') {
+            pass.attr('type', 'text');
+            $('#hidePassphrase').html('Hide');
+        }
+        else {
+            pass.attr('type', 'password');
+            $('#hidePassphrase').html('Show');
+        }
+    }
+
+    var numBits = 256; // this needs to actually be dependent on desired key size, should make at least 256
+    function GeneratePassword() {
+        var passphrase = $('#passphrase').val();
+
+        var salt = sjcl.hash.sha256.hash('brainpassSalt');
+        var passphraseHash = sjcl.misc.pbkdf2(passphrase, salt, kPbkdf2Iteration, numBits);
+        var hashString = sjcl.codec.hex.fromBits(passphraseHash);
+
+        $('#hash').val(hashString);
+        GenerateSitePassword(passphraseHash);
+        ShowVerifier(hashString);
+    }
+
+    function GenerateSitePassword(hash) {
+        var alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        var passwordLength = 20;
+        var bitsLeft = numBits;
+        var base = BigInteger.valueOf(alphabet.length);
+        var password = new Array(passwordLength);
+        var hashString = sjcl.codec.hex.fromBits(hash);
+        var hashBytes = Crypto.util.hexToBytes(hashString);
+        var hashBigNum = BigInteger.fromByteArrayUnsigned(hashBytes);
+        for (var passwordI = 0; passwordI < passwordLength; ++passwordI) {
+            var mod = hashBigNum.mod(base);
+            password[passwordI] = alphabet[mod.intValue()];
+            hashBigNum = hashBigNum.subtract(mod).divide(base);
+        }
+        var passwordStr = password.join('');
+        $('#generatedPassword').val(passwordStr);
+    }
+
+    function ShowVerifier(hashString) {
+        var verifierBytes = sjcl.hash.sha256.hash(hashString);
+        var verifierHex = sjcl.codec.hex.fromBits(verifierBytes);
+        $('#verifier').val(verifierHex);
+    }
+
+    function OnInputChange() {
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            GeneratePassword();
+        }, kTimeoutDuration);
+    }
+
+    function OnInput(id, func) {
+        $(id).bind("input keyup keydown keypress change blur", function() {
+            if ($(this).val() != jQuery.data(this, "lastvalue")) {
+                func();
+            }
+            jQuery.data(this, "lastvalue", $(this).val());
+        });
+        $(id).bind("focus", function() {
+           jQuery.data(this, "lastvalue", $(this).val());
+        });
+    }
+
+    $(document).ready( function() {
+        OnInput('#passphrase', OnInputChange);
+        $('#hidePassphrase').click(showHidePassphrase);
+    });
+})(jQuery);
